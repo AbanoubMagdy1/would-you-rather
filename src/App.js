d9ce5b8af772065ded5133442988027ca0a8f4e5
AbanoubMagdy1 <bebomagdy666@gmail.com>
2021-05-06 21:31:50 +0200

    Prepare App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>App is running</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
